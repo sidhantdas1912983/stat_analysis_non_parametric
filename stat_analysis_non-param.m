d48668e00a8a48e390d44f0caef38bff056b29d7
Sidhant Das <77183109+sidhantdas1912983@users.noreply.github.com>
2023-06-10 18:15:51 -0700

    Update stat_analysis_non-param.m

--- stat_analysis_non-param.m
+++ stat_analysis_non-param.m
@@ -18,7 +18,8 @@ col125 = PS(:,5);
 [p10,v10] = signrank(col100,col125);
 
 %%
-p = [p1; p2; p3; p4;p5;p6;p7;p8;p9;p10 ];
+%p = [p1; p2; p3; p4;p5;p6;p7;p8;p9;p10 ];
+p = [0.001953125;0.001953125;0.000854492;0.000732422;0.234375;0.0625;0.03515625;0.625;0.25;1];
 q = sort(p);
 old_result =zeros(length(p),1);
 
@@ -33,16 +34,58 @@ end
 %%
 fp = 0.05;
 q = sort(p);
-x = zeros(length(q),1);
-h = zeros(length(q),1);
-for i = 1:length(q)
-    x(i) = (fp*(i/length(p)));
-    if x(i)>q(i)
-        h(i) = 1;%%reject null hypothesis : statistically different
-    else
-        h(i) =0;%%%%accept null hypothesis : statistically similar
+if length(unique(q)) == length(q)
+    
+    x = zeros(length(q),1);
+    h = zeros(length(q),1);
+    for i = 1:length(q)
+        x(i) = (fp*(i/length(p)));
+        if x(i)>q(i)
+            h(i) = 1;%%reject null hypothesis : statistically different
+        else
+            h(i) =0;%%%%accept null hypothesis : statistically similar
+        end
     end
+    a = ["25-100";"25-125";"25-75";"50-125";"50-100";"25-50";"50-75";"75-120";"75-100";"100-125"];
+    Summary = ["pairwise p-vals", "sorted p-values", "adjusted p-values", "result";a q x h];
+else
+    Q = benjamin_hochberg_equalp(q);
+    x = zeros(length(q),1);
+    h = zeros(length(q),1);
+    for i = 1:length(q)
+        x(i) = (fp*(Q(i)/length(p)));
+        if x(i)>q(i)
+            h(i) = 1;%%reject null hypothesis : statistically different
+        else
+            h(i) =0;%%%%accept null hypothesis : statistically similar
+        end
+    end
+    a = ["25-100";"25-125";"25-75";"50-125";"50-100";"25-50";"50-75";"75-120";"75-100";"100-125"];
+    Summary = ["pairwise p-vals", "sorted p-values", "adjusted p-values", "result";a q x h];
+    
 end
-a = ["25-100";"25-125";"25-75";"50-125";"50-100";"25-50";"50-75";"75-120";"75-100";"100-125"];
-Summary = ["pairwise p-vals", "sorted p-values", "adjusted p-values", "result";a q x h];
-   
+ 
+function [rankwise_matrix] = benjamin_hochberg_equalp(s)
+    [v,w,rak]= unique(s);
+        turtoise =1;
+        rankwise_matrix=zeros(length(rak),1);
+        hare = turtoise+1;
+        for i = 1:length(rak)-1
+            if(rak(turtoise)==rak(hare)&&turtoise == length(rak)-1)
+                rankwise_matrix(turtoise) = hare;
+                rankwise_matrix(hare) = hare;
+            elseif(rak(turtoise)==rak(hare)&&turtoise ~= length(rak)-1)
+                hare = hare+1;    
+            else
+                rankwise_matrix(turtoise) = turtoise;
+                dif = hare-turtoise-1;
+                if(dif~=0)
+                    rankwise_matrix(turtoise:hare-1,1)=hare-1;    
+                end
+                   turtoise = turtoise+dif+1;
+                   rankwise_matrix(turtoise) = hare;
+                   hare = turtoise+1; 
+
+            end
+        end
+end        
